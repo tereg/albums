cda94d94b8233b55b488edc988fb254d355cc97d
tereg <teregalla@gmail.com>
2017-04-14 14:39:24 -0700

    Import Header to App component.

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,12 @@
 // Import a library to help create a component.
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component.
+// Component nesting: Placing one component inside of another.
 const App = () => (
-  <Text>Some Text</Text>
+  <Header />
 );
 
 
