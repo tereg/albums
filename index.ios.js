a7f5fb19cf1751f90b6c2018101f561213bebf02
tereg <teregalla@gmail.com>
2017-04-15 11:26:49 -0700

    Create AlbumList component
    Create component with dummy text.
    Import View tag to App component.
    Wrap Header and AlbumList components in View tag in App component.

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,16 @@
 // Import a library to help create a component.
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-
+import AlbumList from './src/components/AlbumList';
 // Create a component.
 // Component nesting: Placing one component inside of another.
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
-
 
 // Render it to the device.
 // For every React Native application we create, we must register at least one component.
