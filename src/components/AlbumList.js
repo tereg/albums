ea8857c56751bb18e53f3ffed5470ff6ebdfbd3a
tereg <teregalla@gmail.com>
2017-04-16 18:41:37 -0700

    Add componentWillMount to AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,6 +4,15 @@ import { View, Text } from 'react-native';
 
 // We are borrowing functionality from Component class.
 class AlbumList extends Component {
+  //From ReactNative documentation: 
+    //ComponentWillMount() is invoked immediately before mounting occurs. 
+    //It is called before render(), therefore setting state in this method will not trigger a re-rendering. 
+    //Avoid introducing any side-effects or subscriptions in this method.
+  
+  //componentWillMount initiates some loading of data or HTTP request.   
+  componentWillMount() {
+    console.log('hello');
+  }
   render() {
     return (
       <View>
