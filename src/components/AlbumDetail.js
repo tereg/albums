b9e63e2e3c5785a67fb65789eb2ab4292c8a200d
tereg <teregalla@gmail.com>
2017-04-19 12:50:11 -0700

    Add album image to AlbumDetail component and add styles to text and images

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -15,22 +15,34 @@ import CardSection from './CardSection';
   //to come from the album object. This is a good approach
   //whenever you start making multiple references to your props object. 
 
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const { 
+    thumbnailStyle, 
+    headerContentStyle,
+    thumbnailContainerStyle, 
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image 
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }} 
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle} 
+          source={{ uri: image }} 
+        />
       </CardSection>
     </Card>
   );
@@ -40,10 +52,25 @@ import CardSection from './CardSection';
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
   }, 
   thumbnailStyle: {
     height: 50, 
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center', 
+    marginLeft: 10,
+    marginRight: 10
+  },
+  //flex:1 and width:null makes image fill the whole width
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
  };
 
