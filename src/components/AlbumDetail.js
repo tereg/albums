3efeb064283a47ee3f2795405f013d1fb8374b35
tereg <teregalla@gmail.com>
2017-04-19 14:32:34 -0700

    Import Linking from react-native and add link to amazon pages to Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -15,7 +15,7 @@ import Button from './Button';
   //to come from the album object. This is a good approach
   //whenever you start making multiple references to your props object. 
 
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { 
     thumbnailStyle, 
     headerContentStyle,
@@ -47,7 +47,7 @@ import Button from './Button';
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => console.log(title)} />
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
