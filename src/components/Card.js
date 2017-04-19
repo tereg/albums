2af646de69f39339b95c6c3fa729b36cc9d48c1b
tereg <teregalla@gmail.com>
2017-04-19 10:52:12 -0700

    Add styles to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,26 @@ import { View } from 'react-native';
 //to make something with some nice styling that looks like a card.
 const Card = () => {
   return (
-    <View></View>
+    <View style={styles.containerStyle>
+    </View>
   );
+};
+
+//containerStyle is an arbitrary name. I can call it whatever I want.
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginTop: 10
+  }
 };
 
 export default Card;
